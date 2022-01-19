fcdfbac8f08431e837e6cec6f80650cd050f317a
Alexander Vladyko <vladyko.alexander@gmail.com>
2022-01-19 19:26:15 +0300

    game development done

--- SwiftmeFirstApp/main.swift
+++ SwiftmeFirstApp/main.swift
@@ -5,7 +5,10 @@
 //  Created by Александр Владыко on 19.01.22.
 //
 
-import Foundation
+print("Please, type first value")
+var a = readLine()
+print("Please, type second value")
+var b = readLine()
 
-print("Hello, World!")
-
+let summmed = sum(a, b)
+print("Sum of values = \(summmed)")
