012df234d7f114b4d8a843a6657d290cdb89d363
Alexander Vladyko <vladyko.alexander@gmail.com>
2022-01-23 00:10:30 +0300

    L16 HW2 done

--- SwiftmeFirstApp/func.swift
+++ SwiftmeFirstApp/func.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
-func sum(_ a: Int?, _ b: Int?) -> Int {
-    a! + b!
+func operate(_ a: Int?, _ b: Int?, _ operation: SupportedOperation?) -> Int? {
+    guard let c = a else { return nil }
+    guard let d = b else { return nil }
+    guard let action = operation else { return nil }
+    
+    switch action {
+    case .addition:
+        return c+d
+    case .subtraction:
+        return c-d
+    case .multiplication:
+        return c*d
+    case .division:
+        return c/d
+    case .power:
+        var result = c
+        for _ in 1..<d {
+            result *= result
+        }
+        return result
+    }
 }
--- SwiftmeFirstApp/main.swift
+++ SwiftmeFirstApp/main.swift
@@ -7,17 +7,31 @@
 
 var a: Int?
 var b: Int?
+var operation: SupportedOperation?
+
+enum SupportedOperation: String {
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
+    case power = "^"
+}
 
 repeat {
-    print("Please, type first correct value")
+    print("Please, type first correct value:")
     a = Int(readLine() ?? "")
 } while a == nil
 
 repeat {
-    print("Please, type second correct value")
+    print("Please, type second correct value:")
     b = Int(readLine() ?? "")
 } while b == nil
 
+repeat {
+    print("Please, type supported operation:")
+    operation = SupportedOperation.init(rawValue: String(readLine() ?? ""))
+} while operation == nil
 
-let summmed = sum(a, b)
-print("Sum of values = \(summmed)")
+
+let summmed = operate(a, b, operation)
+print("Result of operation: \(a!) \(operation!.rawValue) \(b!) = \(summmed!)")
